41ae168c5f70012cd3a33688e2cd0f624bf3c85f
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 21:08:23 +0530

    connect entry conroller and router

--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -1,21 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get entries" });
-});
+const {
+  getEntries,
+  createEntry,
+  updateEntry,
+  deleteEntry,
+} = require("../controller/entryController.js");
 
-router.post("/", (req, res) => {
-  res.status(201).json({ message: "Add entry" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update entry id ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete entry id ${req.params.id}` });
-});
+router.route("/").get(getEntries).post(createEntry);
+router.route("/:id").put(updateEntry).delete(deleteEntry);
 
 module.exports = router;
