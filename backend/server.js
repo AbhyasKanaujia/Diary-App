0359542e93bebc96db1400019a7bf60cd8b6ccfc
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 21:11:34 +0530

    add middleware to accept body data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/entries", entryRouter);
 
 app.listen(
