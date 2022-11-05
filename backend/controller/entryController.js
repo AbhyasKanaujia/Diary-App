286638d58369c7df38e77a942f94c95de8a7ff0f
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 21:20:16 +0530

    add express async handler in entry conroller

--- backend/controller/entryController.js
+++ backend/controller/entryController.js
@@ -1,30 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get all entries
 // @route   GET /api/entries
 // @access  Private
-const getEntries = (req, res) => {
+const getEntries = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get entry" });
-};
+});
 
 // @desc    Create an entry
 // @route   POST /api/entries
 // @access  Private
-const createEntry = (req, res) => {
+const createEntry = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "Add entry" });
-};
+});
 
 // @desc    Update an entry
 // @route   PUT /api/entries/:id
 // @access  Private
-const updateEntry = (req, res) => {
+const updateEntry = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update entry id ${req.params.id}` });
-};
+});
 
 // @desc    Delete an entry
 // @route   DELETE /api/entries/:id
 // @access  Private
-const deleteEntry = (req, res) => {
+const deleteEntry = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete entry id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getEntries,
