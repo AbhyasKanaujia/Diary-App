271440f02e4eb6b6a5696b8132a0f76d925078f1
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 21:28:12 +0530

    connect server to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorHandler.js");
 const entryRouter = require("./routes/entryRoutes.js");
 require("dotenv").config();
+const connectDB = require("./config/db.js");
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
