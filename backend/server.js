00005a6d9c0d0ffd7a1db37f86d4cb4420f24d82
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-09 11:19:26 +0530

    add user router to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const { errorHandler } = require("./middlewares/errorHandler.js");
 const entryRouter = require("./routes/entryRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
 require("dotenv").config();
 const connectDB = require("./configs/db.js");
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/entries", entryRouter);
+app.use("/api/users", userRouter);
 
 app.use(errorHandler);
 
