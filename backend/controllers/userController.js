989bdc3f3712bba0640263656766bb00552111c4
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-09 11:50:59 +0530

    implement user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,48 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel.js");
 
 // @desc    Register new userr
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register user" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+
+    throw new Error("Please add all the fields");
+  }
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+
+    throw new Error("User already exists");
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Authenticate user
