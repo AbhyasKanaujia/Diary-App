4f233cbadb35d486fbd8878c77911f20037ffbb9
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-10 23:28:54 +0530

    add initial state to authSlice

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,2 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
