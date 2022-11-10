2a992c53dbbe3f25d8b922e01e7a1368e749d334
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-10 23:22:32 +0530

    create login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,65 @@
+import { useState, useEffect } from "react";
+import { FaUserAlt } from "react-icons/fa";
+
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUserAlt /> Login
+        </h1>
+      </section>
+      <section className="from">
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              className="form-control"
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              className="form-control"
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
